bc6215a794e6ef72255d480ba3daa830ef81caa6
chadwcarlson <chadwcarlson@gmail.com>
2017-05-04 09:18:08 -0400

    animation notation test

---
+++ evolearn/environment/animation.py
@@ -0,0 +1,50 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class Animation:
+
+    def __init__(self, data):
+
+        """General purpose animation class using matplotlib.
+        
+        `Animation` creates an animation object from an arbitrary 3D array.
+        
+        # Example
+        
+        ```python
+            sample = np.random.randn(100, 100, 60)
+            anim = Animation(sample)
+            anim.animate()
+        ```
+        
+        # Input shape
+            numpy array with shape: `(rows, cols, number_of_frames)`.
+            
+        # Output
+            matplotlib Artist Animation object instance. 
+        
+         
+        """
+
+        self.fig = plt.figure()
+        self.frames = [[plt.imshow(data[:, :, frame], animated=True)] for frame in range(data.shape[2])]
+
+        # Set up formatting for saving an animation
+        # self.save_animation = True
+        # Writer = animation.writers['ffmpeg']
+        # self.writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+    def animate(self):
+
+        ani = animation.ArtistAnimation(self.fig, self.frames, interval=50, blit=True, repeat_delay=1000)
+        plt.axis('off')
+        plt.show()
+        # if self.save_animation:
+        #     ani.save('im.mp4', writer=self.writer)
+
+
+##### EXAMPLE #####
+# sample = np.random.randn(100, 100, 60)
+# anim = Animation(sample)
+# anim.animate()
