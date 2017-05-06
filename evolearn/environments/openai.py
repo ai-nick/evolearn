28f5adeaccf9e87661b5edd694ccbef1c756b9b0
chadwcarlson <chadwcarlson@gmail.com>
2017-05-06 15:58:45 -0400

    autodoc check - add dummy openai classes

---
+++ evolearn/environments/openai.py
@@ -0,0 +1,18 @@
+
+class FlappyBird:
+
+    """FlappyBird test class
+
+    :param genome: population of cppn networks.
+    :type genome: NaN
+
+    """
+
+class BalancingPole:
+
+    """FlappyBird test class
+
+    :param genome: population of cppn networks.
+    :type genome: NaN
+
+    """
