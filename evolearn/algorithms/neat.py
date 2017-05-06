16eec724d151fad077f6139487f4a07352a941af
chadwcarlson <chadwcarlson@gmail.com>
2017-05-06 15:48:27 -0400

    update test_requirements for np and plt

---
+++ evolearn/algorithms/neat.py
@@ -0,0 +1,20 @@
+
+
+class NEAT:
+
+    """NEAT test class
+
+    :param genome: population of cppn networks.
+    :type genome: NaN
+
+    """
+
+
+class HyperNEAT:
+
+    """HyperNEAT test class
+
+    :param genome: population of cppn networks.
+    :type genome: NaN
+
+    """
