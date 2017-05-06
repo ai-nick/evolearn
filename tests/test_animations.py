4a5c9e12ca3538c496a931bcb71e740d3f938901
chadwcarlson <chadwcarlson@gmail.com>
2017-05-06 10:20:43 -0400

    index test

--- tests/test_animations.py
+++ tests/test_animations.py
@@ -1,15 +1,14 @@
 
+# ----- Update path for test -----
+
 import os
 import sys
 
-# Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
 project_root = os.path.dirname(cwd)
-
-# Insert the project root dir as the first element in the PYTHONPATH.
-# This lets us ensure that the source package is imported, and that its
-# version is used.
 sys.path.insert(0, project_root)
+
+# ----- TEST -----
 
 from evolearn.utils.visualize import Animation
 import numpy as np
