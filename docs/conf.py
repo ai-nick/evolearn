80dd4b643174f7b17354a5084c6b60435e1f2dc2
chadwcarlson <chadwcarlson@gmail.com>
2017-05-06 09:39:38 -0400

    update test_animations path

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = [ 'numpy', 'matplotlib.pyplot', 'matplotlib.animation', 'networkx', 'itertools' ]
+MOCK_MODULES = [ 'numpy', 'matplotlib.pyplot', 'matplotlib.animation', 'networkx']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
