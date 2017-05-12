d6aa5c2aa8b1a282cb47bb39e1efa596a19d3252
chadwcarlson <chadwcarlson@gmail.com>
2017-05-12 18:23:28 -0400

    working kivy_app_repeat for potential applications/picbreeder

--- tests/test_picbreeder_gui.py
+++ tests/test_picbreeder_gui.py
@@ -1,5 +1,5 @@
 '''
-Repeat Texture on Resize
+Repeat Texture on Resize - Could be used for picbreeder
 ========================
 
 This examples repeats the letter 'K' (mtexture1.png) 64 times in a window.
